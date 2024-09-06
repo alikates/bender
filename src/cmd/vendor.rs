52f534fe09ca708ada4933d7a72d137df3941342
alikates <alikates@dnyon.com>
2024-09-06 18:19:23 +0200

    Fix copying vendored files when they are symbolic links

--- src/cmd/vendor.rs
+++ src/cmd/vendor.rs
@@ -757,7 +757,10 @@ pub fn copy_recursively(
         }
 
         let filetype = entry.file_type()?;
-        let canonical_path_filetype = std::fs::metadata(std::fs::canonicalize(entry.path()).unwrap()).unwrap().file_type();
+        let canonical_path_filetype =
+            std::fs::metadata(std::fs::canonicalize(entry.path()).unwrap())
+                .unwrap()
+                .file_type();
         if filetype.is_dir() {
             copy_recursively(
                 entry.path(),
@@ -766,7 +769,7 @@ pub fn copy_recursively(
                 ignore,
             )?;
         } else if filetype.is_symlink() && canonical_path_filetype.is_dir() {
-            let orig = std::fs::canonicalize(entry.path());
+            let orig = std::fs::read_link(entry.path());
             symlink_dir(orig.unwrap(), destination.as_ref().join(entry.file_name()))?;
         } else {
             std::fs::copy(entry.path(), destination.as_ref().join(entry.file_name())).map_err(
